bb49ef380c264309f01caf1f874cb3499f744482
Tahseen Siddiqui <tahseensiddiqui@Tahseens-MacBook-Pro.local>
2019-06-03 12:15:38 -0700

    add css to students

--- quran-academy-client/src/components/students/Student.js
+++ quran-academy-client/src/components/students/Student.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import StudentInput from './StudentInput'
+import "./student.css"
 
 const Student = ({match, students}) => {
   const id = match.params.studentId -1;
   const student = students[id]
   return (
-    <div key={student.id}>
+    <div className="studentCard" key={student.id}>
     <p>Name: {student.name}</p>
     <p>Age: {student.age}</p>
     <p>Gender: {student.gender} </p>
     <p>Image:
-    <img src="" alt=""/>
+    <img className="studentImg" src="" alt=""/>
     </p>
     <p>Current Lesson: {student.lesson} </p>
     <StudentInput student={student} />
--- quran-academy-client/src/components/students/Students.js
+++ quran-academy-client/src/components/students/Students.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './student.css'
 
 const Students= ({students}) => {
   const renderStudents = students.map(student =>
     <p key={student.id}><Link  key={student.id} to={`/students/${student.id}`}>{ student.name }</Link></p>
   );
   return (
-    <ul>
+    <div className="studentContainer">
       {renderStudents}
-    </ul>
+    </div>
   )
 }
 
